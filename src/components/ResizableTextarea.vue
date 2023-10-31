d9904d39eae9cbb28b1a3902904811ffdbebe054
LaLa-Lisa <lyohich999@ya.ru>
2023-10-31 19:39:55 +0300

    страничка добавления пациента

---
+++ src/components/ResizableTextarea.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <label :for="id" class="block mb-2 text-sm font-medium text-gray-900">{{ label }}</label>
+        <textarea
+            :id="id"
+            :v-model="model"
+            @input="resizeTextarea"
+            ref="textarea"
+            class="ad-input"
+        ></textarea>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      id: String,
+      label: String,
+      model: String,
+    },
+    data() {
+      return {
+        text: '',
+      };
+    },
+    methods: {
+      resizeTextarea() {
+        this.$refs.textarea.style.height = 'auto';
+        this.$refs.textarea.style.height = this.$refs.textarea.scrollHeight + 'px';
+      },
+    },
+  };
+  </script>
