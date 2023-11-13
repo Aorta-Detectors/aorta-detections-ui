8a6a2f0199bc7bda917404b646cb80e9c1c01e65
Boumbanda Pama Gondran A <56638172+madoumadev@users.noreply.github.com>
2023-11-14 02:46:14 +0300

    Просмотр Обследования, Просмотр Отчета

--- src/components/common/PageHeaderComponent.vue
+++ src/components/common/PageHeaderComponent.vue
@@ -1,12 +1,16 @@
 <script setup lang="ts">
 defineProps({
-  title: String
+  title: String,
+  description: String
 })
 </script>
 
 <template>
-  <div class="flex justify-between items-center w-full">
-    <h1 class="font-bold text-xl lg:text-2xl">{{ title }}</h1>
+  <div class="flex justify-between lg:flex-row flex-col lg:items-center lg:space-y-0 space-y-4 w-full">
+    <div>
+      <h1 class="font-bold text-xl lg:text-2xl">{{ title }}</h1>
+      <span class='text-gray-500'>{{description}}</span>
+    </div>
     <slot />
   </div>
 </template>
